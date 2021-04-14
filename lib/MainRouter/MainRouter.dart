3a2383a66185c9067fa35f22f21075f6f7ddff59
Aleksandar Geyman <ageyman@appolica.com>
2021-04-14 10:49:02 +0300

    Add: Created Main Router

---
+++ lib/MainRouter/MainRouter.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation_app/CustomWidgets/CustomElevatedButton.dart';
+import 'package:flutter_navigation_app/main.dart';
+
+class Constants {
+  static const title = 'Main Router';
+  static const buttonText = 'Push';
+}
+
+class MainRouter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(Constants.title),
+        ),
+        body: Center(
+          child: _buildList(),
+        ));
+  }
+
+  Widget _buildList() {
+    return ListView(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      shrinkWrap: true,
+      children: [
+        CustomElevatedButton(
+            Routes.secondRoute, Navigate.push, Routes.secondRoute),
+        CustomElevatedButton(
+            Routes.secondRoute, Navigate.push, Routes.secondRoute),
+        CustomElevatedButton(
+            Routes.secondRoute, Navigate.push, Routes.secondRoute),
+        CustomElevatedButton(
+            Routes.secondRoute, Navigate.push, Routes.secondRoute),
+        CustomElevatedButton(
+            Routes.secondRoute, Navigate.push, Routes.secondRoute)
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_navigation_app/FirstScreen/FirstScreen.dart';
 
+import 'MainRouter/MainRouter.dart';
 import 'SecondScreen/SecondScreen.dart';
 
 class Routes {
-  static const firstRoute = 'First Route';
-  static const secondRoute = 'Push';
+  static const mainRoute = '/';
+  static const secondRoute = 'Second Route';
   static const thirdRoute = '';
   static const forthRoute = '';
 
   static final appRoutes = {
-    Routes.firstRoute: (context) => FirstRoute(),
+    Routes.mainRoute: (context) => MainRouter(),
     Routes.secondRoute: (context) => SecondRoute(),
   };
 }
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: Routes.firstRoute,
+    initialRoute: Routes.mainRoute,
     routes: Routes.appRoutes,
   ));
 }
